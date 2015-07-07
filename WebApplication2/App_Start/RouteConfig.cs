cd5b01b946f7eccef6b9740dd9fc0f863fc51d1b
ermanyilmaz <ermanyilmz@gmail.com>
2015-07-07 11:35:35 +0300

    v1Lecture1

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebApplication2.Controllers;
 
 namespace WebApplication2
 {
@@ -11,9 +12,13 @@ namespace WebApplication2
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new [] {typeof(PostsController).Namespace};
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //      nameoftheRouter ,pathOfURL,an object that contains default route values
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" },namespaces);
+            routes.MapRoute("Home", "", new { controller="Posts", action="Index" },namespaces);
 
-            routes.MapRoute("Home", "", new { controller="Posts", action="Index" });
+
           
         }
     }
