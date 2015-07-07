b0e1f644b3d66055e090322ee41503af3b81aa3c
ermanyilmaz <ermanyilmz@gmail.com>
2015-07-07 10:43:22 +0300

    vRouting

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -13,11 +13,8 @@ namespace WebApplication2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { controller="Posts", action="Index" });
+          
         }
     }
 }
