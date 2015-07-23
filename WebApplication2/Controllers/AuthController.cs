3c0c9e5cc46ef45ddb73381ad48e3618f7374b5e
ermanyilmaz <ermanyilmz@gmail.com>
2015-07-23 14:25:55 +0300

    Very Basic Login Form

--- WebApplication2/Controllers/AuthController.cs
+++ WebApplication2/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.ViewModels;
 
 namespace WebApplication2.Controllers
 {
@@ -12,6 +13,20 @@ namespace WebApplication2.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+           if(!ModelState.IsValid)
+            return View(form);
+
+           if (form.Username != "quarizma")
+           {
+               ModelState.AddModelError("Username", "Go and fun yourself !");
+               return View(form);
+           }
+
+           return Content("The form is valid!");
         }
     }
 }
