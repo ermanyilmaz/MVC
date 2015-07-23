41ce271fb1635faedba63647ed0d0a6717e15db4
ermanyilmaz <ermanyilmz@gmail.com>
2015-07-23 17:58:17 +0300

    Implemented form Authentication and custom role provider

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace WebApplication2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             //      nameoftheRouter ,pathOfURL,an object that contains default route values
             routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
             routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
 
             routes.MapRoute(
--- WebApplication2/Controllers/AuthController.cs
+++ WebApplication2/Controllers/AuthController.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebApplication2.ViewModels;
 
 namespace WebApplication2.Controllers
 {
     public class AuthController : Controller
     {
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
         // GET: Auth
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin { });
         }
         [HttpPost]
-        public ActionResult Login(AuthLogin form)
+        public ActionResult Login(AuthLogin form, string returnUrl)
         {
            if(!ModelState.IsValid)
             return View(form);
+            // this is the authentication which determines if a person is who says he is.
+           FormsAuthentication.SetAuthCookie(form.Username, true); // All the encryption and hashing and validation is made by asp.net
 
-           if (form.Username != "quarizma")
-           {
-               ModelState.AddModelError("Username", "Go and fun yourself !");
-               return View(form);
-           }
+           if (!string.IsNullOrWhiteSpace(returnUrl))
+               return Redirect(returnUrl);
 
-           return Content("The form is valid!");
+           return RedirectToRoute("home");
+
         }
     }
 }
