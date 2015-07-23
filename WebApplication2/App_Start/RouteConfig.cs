3ed9aad464374447b944451fa67fd179f69b9d66
ermanyilmaz <ermanyilmz@gmail.com>
2015-07-23 11:02:31 +0300

    CompChanges

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -12,14 +12,19 @@ namespace WebApplication2
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var namespaces = new [] {typeof(PostsController).Namespace};
+            var namespaces = new[] { typeof(PostsController).Namespace };
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             //      nameoftheRouter ,pathOfURL,an object that contains default route values
-            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" },namespaces);
-            routes.MapRoute("Home", "", new { controller="Posts", action="Index" },namespaces);
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+
+            routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Auth", action = "Login", id = UrlParameter.Optional }
+);
 
 
-          
         }
     }
 }
